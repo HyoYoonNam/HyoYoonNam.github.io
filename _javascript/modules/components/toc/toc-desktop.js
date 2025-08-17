84290140393fa9282f13d1e9043417350e2173b1
HyoYoonNam <dev.rudevico@gmail.com>
2025-08-17 21:41:14 +0900

    chore: 목차(toc) 자동으로 접히지 않도록 설정

--- _javascript/modules/components/toc/toc-desktop.js
+++ _javascript/modules/components/toc/toc-desktop.js
@@ -5,6 +5,12 @@ export class TocDesktop {
     contentSelector: '.content',
     ignoreSelector: '[data-toc-skip]',
     headingSelector: 'h2, h3, h4',
+    // How many heading levels should not be collapsed.
+    // For example, number 6 will show everything since
+    // there are only 6 heading levels and number 0 will collapse them all.
+    // The sections that are hidden will open
+    // and close as you scroll to headings within them.
+    collapseDepth: 6, // 모든 heading 수준(h1~h6)을 펼칩니다. 하위 목차가 자동으로 접히지 않게 설정.
     orderedList: false,
     scrollSmooth: false,
     headingsOffset: 16 * 2 // 2rem
