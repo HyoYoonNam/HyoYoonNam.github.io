daa912c8415658f98bbd184dfb3035df9d9b4135
HyoYoonNam <dev.rudevico@gmail.com>
2025-12-07 22:09:01 +0900

    hits 조회수 기능 추가

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -5,12 +5,37 @@ import {useBlogPost} from '@docusaurus/plugin-content-blog/client';
 
 export default function BlogPostItemWrapper(props) {
   const { metadata, isBlogPostPage } = useBlogPost();
+  const siteUrl = 'https://hyoyoonnam.github.io'; 
+
+  // hits.sh 서비스 사용
+  const targetPath = (siteUrl + metadata.permalink).replace('https://', '');
+  const hitsImageUrl = `https://hits.sh/${targetPath}.svg?view=today-total&style=flat-square&label=hits&color=79C83D`;
 
   return (
     <>
+      {/* ------------------------------------------------------- */}
+      {/* [1] 조회수 뱃지 (상세 페이지에서만 표시!)                  */}
+      {/* ------------------------------------------------------- */}
+      {isBlogPostPage && (
+        <div style={{display: 'flex', justifyContent: 'flex-end', marginBottom: '10px'}}>
+          <a href={`https://hits.sh/${targetPath}/`} title="조회수 상세 보기" style={{textDecoration: 'none'}}>
+            <img
+              src={hitsImageUrl}
+              alt="hits"
+              style={{ display: 'inline-block', height: '20px', verticalAlign: 'middle' }} 
+            />
+          </a>
+        </div>
+      )}
+
+      {/* ------------------------------------------------------- */}
+      {/* [2] 본문                                                */}
+      {/* ------------------------------------------------------- */}
       <BlogPostItem {...props} />
       
-      {/* 상세 페이지(isBlogPostPage)일 때만 댓글창 표시 */}
+      {/* ------------------------------------------------------- */}
+      {/* [3] 댓글창 (상세 페이지에서만 표시)                       */}
+      {/* ------------------------------------------------------- */}
       {isBlogPostPage && (
         <div style={{marginTop: '50px'}}>
           <Giscus
